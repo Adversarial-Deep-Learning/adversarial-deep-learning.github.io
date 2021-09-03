5cf1af89f17cb357e11dedb0bd1304bcfc5a5233
blackrosedragon2 <helloharshit68@gmail.com>
2021-09-03 21:00:27 +0530

    Add Website Generation Scripts

---
+++ adl/ch5/GAN_Tutorial.md
@@ -0,0 +1,7 @@
+---
+layout: default
+title: GAN_Tutorial
+parent: ch5
+grand_parent: Adversarial Deep Learning
+---
+{% include_relative GAN/GAN_Tutorial.html %}
---
+++ scripts/WebsiteGeneration/GenerateWebsite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ./adl_tutorials 
+
+bash ./scripts/WebsiteGeneration/SparseDownloader.sh
+# files should not have spaces !
+python3 ./scripts/WebsiteGeneration/generation_script.py
+echo "Done generating the website"
+rsync -va adl_tutorials/Tutorials/ adl/
+rm -rf ./adl_tutorials 
